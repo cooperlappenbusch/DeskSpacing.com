22cd56fc36ff3991b323121931c2057ca504a4af
cooperlappenbusch <coopertrooper7@gmail.com>
2020-06-30 13:54:52 -0700

    Added custom monitor selection on the website, but the selection on THREE.js side doesn't work yet. Working on that next.

--- dropDownLogic.js
+++ dropDownLogic.js
@@ -1,5 +1,20 @@
-$(function () {
-    $("#dropdown a").click(function () {
-        $("#AdvancedSearch .selection").text('Aspect Ratio: ' + $(this).text());
-    });
-});
+var dropDownDict = {
+    "16:9": ["23\"", "24\""],
+    "21:9": ["34\"", "27\""],
+    "32:9": ["49\""]
+}
+var aspectRatioForm = document.getElementById("aspectRatioForm");
+var monitorSizeForm = document.getElementById("monitorSizeForm");
+
+aspectRatioForm.addEventListener("click", resetCustomAspectRatio, false);
+
+function resetCustomAspectRatio(e) {
+    monitorSizeForm.innerHTML = '';
+    console.log(aspectRatioForm.value);
+    for (var i = 0; i < dropDownDict[aspectRatioForm.value].length; i++) {
+        var newSize = document.createElement('option');
+        newSize.value = dropDownDict[aspectRatioForm.value][i];
+        newSize.innerHTML = dropDownDict[aspectRatioForm.value][i];
+        monitorSizeForm.appendChild(newSize);
+    }
+}
